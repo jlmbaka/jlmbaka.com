ef6d5e103024a27f845aeb4706bd2cd7800ce48f
Jean-Louis <jeanlouis.mbaka@gmail.com>
2021-06-02 12:12:31 +0100

    Adjust footer repsonsive padding

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,21 +1,28 @@
 import React from "react"
 import Navbar from "./Navbar"
 import "../styles/global.scss"
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
 
 export default function Layout({ children }) {
   return (
-    <>
-      <div className="layout">
-        <Navbar />
-        <div className="content">{children}</div>
-      </div>
-      <footer>
-        <div className="footer__container">
-          <p>© 2021 Jean-Louis Mbaka</p>
-          <p>hello@jlmbaka.com</p>
-          <p>Kinshasa, DRC</p>
-        </div>
-      </footer>
-    </>
+    <Container fluid>
+      <Row>
+        <Col>
+          <div className="layout">
+            <Navbar />
+            <div className="content">{children}</div>
+          </div>
+          <footer>
+            <div className="footer__container">
+              <p>© 2021 Jean-Louis Mbaka</p>
+              <p>hello@jlmbaka.com</p>
+              <p>Kinshasa, DRC</p>
+            </div>
+          </footer>
+        </Col>
+      </Row>
+    </Container>
   )
 }
