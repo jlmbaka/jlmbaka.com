6b971679bfc62c84e024d1f7c5bc82ab27206854
Jean-Louis <jeanlouis.mbaka@gmail.com>
2021-06-05 13:11:49 +0100

    Adjusts index columns & button top margin

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import styled from "styled-components"
 
 const Style = styled.div`
   .hero {
-    padding: 17vh 0 7vh 0;
+    padding: 29vh 0 7vh 0;
   }
 
   .hero--textblock {
@@ -21,8 +21,8 @@ const Style = styled.div`
 
 const Button = styled(Link)`
   display: inline-block;
-  padding: 16px 24px;
-  margin-top: 20px;
+  padding: 1rem 2rem;
+  margin-top: 1.5rem;
   color: white;
   background-color: #222020;
   border: 2px solid #222020;
@@ -65,7 +65,7 @@ export default function Home({ data }) {
     <Layout>
       <Style>
         <section className="hero row">
-          <div className="hero--textblock order-2 order-lg-1 col-lg offset-md-3">
+          <div className="hero--textblock order-2 order-lg-1 col-lg-4 offset-md-3">
             <h1>Jean-Louis Mbaka</h1>
             <Paragraph>
               I'm a Co-Founder at{" "}
@@ -82,7 +82,7 @@ export default function Home({ data }) {
               <FontAwesomeIcon icon={faChevronRight} color="white" />
             </Button>
           </div>
-          <div className="hero--banner order-1 order-lg-2 col-lg offset-md-3 offset-lg-0">
+          <div className="hero--banner order-1 order-lg-2 col-lg-4 offset-md-3 offset-lg-0">
             <GatsbyImage
               image={data.file.childImageSharp.gatsbyImageData}
               alt="site banner"
