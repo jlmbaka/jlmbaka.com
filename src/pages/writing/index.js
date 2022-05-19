df0fa93263859f5cda23c6a0df1ab34becb1bd59
unknown <jeanlouis.mbaka@gmail.com>
2022-05-19 22:28:02 +0100

    Adds stories for writings and handle empty state

--- src/pages/writing/index.js
+++ src/pages/writing/index.js
@@ -1,37 +1,7 @@
 import { graphql } from "gatsby"
-import React from "react"
-import InternalPageLayout from "../../components/InternalPageLayout"
-import ContentList from "../../components/ContentList"
+import Writing from "./view"
 
-export default function Writing({ data }) {
-  const writings = data.writings.nodes.map(writing => ({
-    ...writing.frontmatter,
-    id: writing.id,
-  }))
-  const years = Array.from(
-    new Set(
-      writings.map(writing => {
-        const date = new Date(writing.date)
-        return date.getFullYear()
-      })
-    )
-  )
-
-  return (
-    <InternalPageLayout>
-      <h1 className="mb-5">Writings</h1>
-      {years.map(year => (
-        <ContentList
-          key={year}
-          year={year}
-          baseUrl="writings"
-          showDate
-          items={writings.filter(writing => writing.date.startsWith(year))}
-        />
-      ))}
-    </InternalPageLayout>
-  )
-}
+export default Writing
 
 export const query = graphql`
   query WritingsPage {
